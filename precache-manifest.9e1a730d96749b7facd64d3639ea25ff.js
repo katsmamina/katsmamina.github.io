3642594d875ea06886ca90bd1e1d21cc9a8f404d
katsmamina <katsmamina@yandex.ru>
2022-02-13 23:00:13 +0100

    Updates

---
+++ precache-manifest.9e1a730d96749b7facd64d3639ea25ff.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47dc22879630837524081b1dc55350bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41eca80dfbf20d37b1b4",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "3c55bfcc86b8d3945194",
+    "url": "/static/css/main.4331c200.chunk.css"
+  },
+  {
+    "revision": "41eca80dfbf20d37b1b4",
+    "url": "/static/js/2.584fdfe5.chunk.js"
+  },
+  {
+    "revision": "e13ee005b20792bf401a8ddd8bd5f6e2",
+    "url": "/static/js/2.584fdfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972b8a97c69d14d6854f",
+    "url": "/static/js/3.ee11d966.chunk.js"
+  },
+  {
+    "revision": "3c55bfcc86b8d3945194",
+    "url": "/static/js/main.4c5f9341.chunk.js"
+  },
+  {
+    "revision": "a5b15f76c524ec649f69",
+    "url": "/static/js/runtime-main.c2e187b4.js"
+  },
+  {
+    "revision": "f63ada20353a0fd2d4c64468b047fe65",
+    "url": "/static/media/BeHelp.f63ada20.jpeg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "09e4ec27756a18c936f92f4e2957e822",
+    "url": "/static/media/css-3.09e4ec27.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/static/media/git.1190a19c.svg"
+  },
+  {
+    "revision": "7dffd9f3befc41d7496ce325f3055084",
+    "url": "/static/media/html5.7dffd9f3.svg"
+  },
+  {
+    "revision": "b2b629d0b0a96a0b124bd96763c333e6",
+    "url": "/static/media/loading.b2b629d0.svg"
+  },
+  {
+    "revision": "ca3520d885bb4fda93f6bc154f85a738",
+    "url": "/static/media/myphoto.ca3520d8.JPEG"
+  },
+  {
+    "revision": "312699aa50f42ed83a8eda33ab8977dc",
+    "url": "/static/media/nodejs.312699aa.svg"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/static/media/react-1.876a8325.svg"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/static/media/sass.470548b4.svg"
+  },
+  {
+    "revision": "22efb7c22774c39c790448f62bda240d",
+    "url": "/static/media/vuejs.22efb7c2.svg"
+  }
+]);
